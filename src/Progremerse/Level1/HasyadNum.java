29c94afa75c3b6217de1d3df58d3b82e7621746b
Kimyeongsan <apua34@gmail.com>
2021-08-26 17:06:13 +0900

    Level 1 HidePhoneNumber Add

---
+++ src/Progremerse/Level1/HasyadNum.java
@@ -0,0 +1,13 @@
+package Progremerse.Level1;
+
+public class HasyadNum {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+       if(x > 9) {
+    	   
+       } 
+        
+        return answer;
+    }
+}
