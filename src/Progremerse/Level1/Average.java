9d88305781b9f6a0a68de168c8e2a1f81ee2725f
Kimyeongsan <apua34@gmail.com>
2021-08-31 13:46:58 +0900

    Average & NumAndWorld

---
+++ src/Progremerse/Level1/Average.java
@@ -0,0 +1,36 @@
+package Progremerse.Level1;
+
+import java.util.Scanner;
+
+public class Average {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double sum = 0;
+        
+        for(int i = 0; i < arr.length; i++) {
+        	sum += arr[i];
+        	
+    		System.out.println(sum);
+        }
+        
+        answer = sum / arr.length;
+     
+        return answer;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] names = new int[4];
+		
+		Average result = new Average();
+		
+		Scanner sc = new Scanner(System.in);
+
+		for (int i = 0; i < names.length; i++) {
+			names[i] = sc.nextInt();
+		}
+		
+		System.out.print(result.solution(names));
+	
+	}
+
+}
