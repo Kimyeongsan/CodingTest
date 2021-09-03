4ab2635778f881048cb2bde8064991e711511896
Kimyeongsan <apua34@gmail.com>
2021-09-03 21:25:26 +0900

    Reflesh

--- src/Progremerse/Level2/EatTheGround.java
+++ src/Progremerse/Level2/EatTheGround.java
@@ -1,55 +1,32 @@
 package Progremerse.Level2;
 
 public class EatTheGround {
-	int hopscotch(int[][] land, int n) {
-		int answer = 0;
-		int[][] dx = { { 1, 2, 3 }, { -1, 1, 2 }, { -2, -1, 1 }, { -3, -2, -1 } };
+   int hopscotch(int[][] land, int n) {
+      int answer = 0;
+      int[][] dx = { { 1, 2, 3 }, { -1, 1, 2 }, { -2, -1, 1 }, { -3, -2, -1 } };
 
-		for (int i = 1; i < land.length; i++) {
-			for (int j = 0; j < 4; j++) {
-<<<<<<< HEAD
-				land[i][j] += Math.max(land[i - 1][j + dx[j][0]], Math.max(land[i - 1][j + dx[j][1]], land[i - 1][j + dx[j][2]]));
-				System.out.println("과정" + land[i][j]);
-			}
-		}
+      for (int i = 1; i < land.length; i++) {
+         for (int j = 0; j < 4; j++) {
+            land[i][j] += Math.max(land[i - 1][j + dx[j][0]],
+                  Math.max(land[i - 1][j + dx[j][1]], land[i - 1][j + dx[j][2]]));
+         }
+      }
+      
+      for (int i = 0; i < land[n - 1].length; i++) {
+         if (answer < land[n - 1][i]) {
+            answer = land[n - 1][i];
+         }
+      }
 
-		for (int i = 0; i < land[n - 1].length; i++) {
-			if (answer < land[n - 1][i]) {
-				answer = land[n - 1][i];
-			}
-		}
+      return answer;
+   }
 
-=======
-				land[i][j] += Math.max(land[i - 1][j + dx[j][0]],
-						Math.max(land[i - 1][j + dx[j][1]], land[i - 1][j + dx[j][2]]));
-			}
-		}
-		
-		for (int i = 0; i < land[n - 1].length; i++) {
-			if (answer < land[n - 1][i]) {
-				answer = land[n - 1][i];
-			}
-		}
+   public static void main(String[] args) {
+      EatTheGround c = new EatTheGround();
 
->>>>>>> c02d8481b7061b55e75b45b6cbd7729513802640
-		return answer;
-	}
-
-	public static void main(String[] args) {
-		EatTheGround c = new EatTheGround();
-
-<<<<<<< HEAD
-		int[][] test = { { 1, 2, 3, 5 }, { 5, 6, 7, 100 }, { 4, 3, 2, 1 } };
-		// 아래는 테스트로 출력해 보기 위한 코드입니다.
-		System.out.println("답" + c.hopscotch(test, 3));
-	}
+      int[][] test = { { 1, 2, 3, 5 }, { 5, 6, 7, 8 }, { 4, 3, 2, 1 } };
+      // 아래는 테스트로 출력해 보기 위한 코드입니다.
+      System.out.println(c.hopscotch(test, 3));
+   }
 
 }
-=======
-		int[][] test = { { 1, 2, 3, 5 }, { 5, 6, 7, 8 }, { 4, 3, 2, 1 } };
-		// 아래는 테스트로 출력해 보기 위한 코드입니다.
-		System.out.println(c.hopscotch(test, 3));
-	}
-
-}
->>>>>>> c02d8481b7061b55e75b45b6cbd7729513802640
