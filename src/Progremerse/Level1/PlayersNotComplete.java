30f536ae3a85686e0e766ce17029e6818ca9cb82
Kimyeongsan <apua34@gmail.com>
2021-09-14 14:37:16 +0900

    NonCompletePlayer Add

--- src/Progremerse/Level1/PlayersNotComplete.java
+++ src/Progremerse/Level1/PlayersNotComplete.java
@@ -2,34 +2,34 @@ package Progremerse.Level1;
 
 import java.util.Arrays;
 
-
 // 완주하지 못한자
 public class PlayersNotComplete {
 
-	public String solution(String[] participant, String[] completion) {
+    public static String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for (int i = 0; i<completion.length; i++){
 
-		Arrays.sort(participant);
-		Arrays.sort(completion);
+            // 같지 않은걸 발견하면 Return
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
 
-		int i;
-		
-		for (i = 0; i < participant.length; i++) {
-			if (!participant[i].equals( completion[i])) {
-				return participant[i];
-			}
-		}
-
-		return participant[i];
-	}
+        // 발견하지 못하고 for문을 나오면 배열의 마지막 값 Return
+        return participant[participant.length - 1];
+    }
 
 	public static void main(String[] args) {
-
-		PlayersNotComplete playersNotComplete = new PlayersNotComplete();
+		
 
 		String[] participant = { "leo", "kiki", "eden" };
 		String[] completion = { "eden", "kiki" };
+		
+		String result = solution(participant, completion);
 
-		System.out.print(playersNotComplete.solution(participant, completion));
+		System.out.print(result);
 
 	}
 
