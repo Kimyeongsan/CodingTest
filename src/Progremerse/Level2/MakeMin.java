0a5da636c457d301e74d31191fed46fff2240bb5
Kimyeongsan <apua34@gmail.com>
2021-10-06 12:01:31 +0900

    문자열 내 마음대로 정렬하기 Add

--- src/Progremerse/Level2/MakeMin.java
+++ src/Progremerse/Level2/MakeMin.java
@@ -1,5 +1,18 @@
 package Progremerse.Level2;
 
-public class MakeMin {
+import java.util.*;
 
+// 최솟값 만들기
+public class MakeMin {
+    public int solution(int[] A, int[] B) {
+		Arrays.sort(A); 
+		Arrays.sort(B);
+		
+		int sum = 0;
+		for(int i = 0; i < A.length; i++) { 
+			sum += A[i] * B[A.length - i - 1];
+            System.out.println(sum);
+		}
+		return sum;
+	}
 }
--- src/Progremerse/Level2/MaxMin.java
+++ src/Progremerse/Level2/MaxMin.java
@@ -1,10 +1,27 @@
 package Progremerse.Level2;
 
+import java.util.*;
+
+// 최댓값과 최솟값
+// Integer.parseInt : int로 변환
+// Ingeger.toString : String으로 변환
+
 public class MaxMin {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public String solution(String s) {
+        String[] num = s.split(" ");
+        
+        int[] list = new int[num.length];
+        
+        for(int i = 0; i < num.length; i++) {
+            list[i] += Integer.parseInt(num[i]);
+        }
+            
+        Arrays.sort(list);
+        
+        int max = list[num.length - 1]; 
+        int min = list[0];
+        
+        return Integer.toString(min) + " " + Integer.toString(max);
+    }
 
 }
